304f167422b66b59eb2325d81287e94432ad67ab
xuantuyen <xuantuyennguyen88@gmail.com>
2017-09-28 16:00:38 +0700

    screen sharing

---
+++ chrome-addon/content-script.js
@@ -0,0 +1,13 @@
+﻿var port = chrome.runtime.connect();
+
+port.onMessage.addListener(function (message) {
+    window.postMessage(message, '*');
+});
+
+window.addEventListener('message', function (event) {
+    if( event.data === 'check-addon-installed' ) {
+        window.postMessage( 'addon-installed', '*' );
+    } else if (event.source === window) {
+        port.postMessage( event.data );
+    }
+});
